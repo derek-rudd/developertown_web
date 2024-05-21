90ec112e5b4157a11d6ff22653c03c8e0398961d
derek-rudd <drudd@developertown.com>
2024-05-21 11:19:44 -0400

    Update cdn-test.js

--- cdn-test.js
+++ cdn-test.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   function init() { 
     document.getElementById("Hero-Heading").style.color = 'blue';
   }
-  console.log("js has been loaded");
+  console.log("js has been loaded - update test");
   init();
   
 });
